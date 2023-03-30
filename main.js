fe04cdbdc64a12873bc271c45e56f2f6ff1ef78d
Gabriela Yasmin <84574158+gabyyasmindesouza@users.noreply.github.com>
2023-03-29 21:40:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+//-------Atividade Array
+// 1. Faça um programa, seguindo os passos:
+    
+//a) Crie um array vazio e guarde-o em uma variável, chamada `tarefas`
+
+//const tarefas = []
+  
+
+//b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+
+//const tarefa1 = prompt("Qual a primeira tarefa?")
+//tarefas.push(tarefa1)
+
+
+//const tarefa2 = prompt("Qual a segunda tarefa?")
+//tarefas.push(tarefa2)
+
+
+//const tarefa3 = prompt("Qual a terceira tarefa?")
+//tarefas.push(tarefa3)
+  
+
+// c) Imprima o array no console
+
+//console.log(tarefas)
+  
+
+//d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2
+
+//const indice = Number(prompt("Dígite o índice da tarefa já realizada"))
+
+    
+//e) Remova da lista o item de índice que o usuário escolheu.
+
+//tarefas.splice(-1, 1)
+  
+
+//f) Imprima o array no console
+
+//console.log(tarefas)
+
+
+// -------Atividade-----    
+
+// A) Escreva uma função que receba 4 parâmetros que correspondem às informações de uma pessoa: o nome, a idade, endereço e uma 
+//profissão. Ela deve retornar uma string que unifique todas as informações da pessoa em uma só mensagem.
+
+//const nome = prompt("Qual seu nome?")
+//const idade = Number(prompt("Qual sua idade?"))
+//const endereco = prompt("Qual seu endereço?")
+//const profissao = prompt("Qual sua profissão?")
+
+//console.log(`Seu nome é ${nome}, tem ${idade} anos, mora no endereço ${endereco} e atua na profissão de ${profissao}`)
+
+
+// B) Escreva uma função que receba 2 números como parâmentros, e, dentro da função, faça a soma das duas entradas e retorne o 
+//resultado. Invoque a função e imprima no console o resultado.
+
+//const numero1 = Number(prompt("Qual o primeiro número?"))
+//const numero2 = Number(prompt("Qual o segundo número?"))
+
+//const numero3 = numero1 + numero2
+//console.log(numero3)
+
+
+// C) Faça uma função que recebe 2 números e retorne um booleano que informa se o primeiro número é **maior ou igual** ao segundo.
+
+
+// D) Escreva uma função que receba um número e devolva um booleano indicando se ele é par ou não
+
+
+// E) Faça uma função que recebe uma mensagem (`string`) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma 
+//versão dela em letras maiúsculas.
+
+//let frase = prompt("Qual a frase?")
+//console.log(frase.length)
+
+
+//Crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão). Em seguida, peça para o usuário 
+//inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário sendo o argumento.b
